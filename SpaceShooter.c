7ec6b28268d48d6d4e2948d334c1fe76067a235a
LetterIce <421juanda@gmail.com>
2024-12-02 12:39:44 +0700

    update to v2-test
    simplified main game function

--- SpaceShooter.c
+++ SpaceShooter.c
@@ -4,21 +4,8 @@
 #include <stdbool.h>
 #include "constants.h"
 
-
-#define MAX_BULLETS 90
+#define MAX_BULLETS 10
 #define MAX_ENEMIES 5
-#define MAX_EXPLOSIONS 90
-
-#define SPACESHIP_SPEED 4
-#define BULLET_SPEED 5
-#define ENEMY_SPEED 2
-#define EXPLOSION_FRAME_COUNT 5
-
-typedef enum {
-    STATE_MENU,
-    STATE_PLAY,
-    STATE_GAME_OVER
-} GameState;
 
 typedef struct {
     int x, y;
@@ -31,186 +18,161 @@ typedef struct {
 } Enemy;
 
 typedef struct {
+    Sprite* sprite;
     int x, y;
-    bool active;
-    int frame;
-} Explosion;
+} GameObject;
 
 typedef struct {
-    Image spaceship;
-    Image enemy;
-    Image explosionEffect;
+    GameObject ship;
+    GameObject enemy;
+    GameObject explosion;
+    GameObject laserBolt;
     Bullet bullets[MAX_BULLETS];
     Enemy enemies[MAX_ENEMIES];
-    Explosion explosions[MAX_EXPLOSIONS];
-    int bulletIndex;
-    int enemyIndex;
-    int explosionIndex;
-    int x, y; // Spaceship position
     int lives;
     int score;
-    GameState gameState;
-
-    // Audio assets
-    Audio explodeSound;
-    Audio hitHurtSound;
-    Audio laserSound;
-    Audio music;
+    int bulletIndex;
+    Color* backgroundColor;
+    u_long* data[8];
 } Game;
 
 Game game;
 
-// Function prototypes
 void initialize();
-void drawMenu();
-void drawGame();
-void updateGame();
-void handleInput();
-void resetGame();
-void gameOver();
-void initializeBullets();
-void initializeEnemies();
-void initializeExplosions();
 void loadAssets();
-void cleanupAssets();
+void update();
+void draw();
 void fireBullet();
 void updateBullets();
 void updateEnemies();
-void updateExplosions();
-void drawExplosions();
-bool checkCollision(int objX, int objY, int objWidth, int objHeight, Bullet bullet);
-void checkBulletCollisions();
-void checkEnemyCollisions();
-void triggerExplosion(int x, int y);
+void checkCollisions();
+bool checkCollision(int x1, int y1, int w1, int h1, int x2, int y2, int w2, int h2);
 
 int main() {
     initialize();
 
     while (1) {
-        switch (game.gameState) {
-            case STATE_MENU:
-                drawMenu();
-                handleInput();
-                break;
-            case STATE_PLAY:
-                updateGame();
-                drawGame();
-                break;
-            case STATE_GAME_OVER:
-                gameOver();
-                handleInput();
-                break;
-        }
+        update();
+        clear_display();
+        draw();
+        display();
     }
 
-    cleanupAssets();
     return 0;
 }
 
 void initialize() {
-    initializeScreen();
-    initializePad();
-    initializeBullets();
-    initializeEnemies();
-    initializeExplosions();
+    initialize_heap();
+    initialize_screen();
+    initialize_pad();
+
+    // Set background color to dark
+    color_create(0, 0, 0, &game.backgroundColor);
+    set_background_color(game.backgroundColor);
+
     loadAssets();
 
-    game.x = SCREEN_WIDTH / 2;
-    game.y = SCREEN_HEIGHT - 50;
+    // Initialize game variables
+    game.ship.x = SCREEN_WIDTH / 2;
+    game.ship.y = SCREEN_HEIGHT - 50;
     game.lives = 3;
     game.score = 0;
-    game.gameState = STATE_MENU;
-    resetGame();
+    game.bulletIndex = 0;
+
+    for (int i = 0; i < MAX_BULLETS; i++) {
+        game.bullets[i].active = false;
+    }
+
+    for (int i = 0; i < MAX_ENEMIES; i++) {
+        game.enemies[i].active = false;
+    }
+
+    // Play background music
+    audio_transfer_vag_to_spu((u_char*)game.data[7], SECTOR * 21, SPU_01CH);
+    audio_play(SPU_01CH);
 }
 
 void loadAssets() {
-    game.spaceship = createImage(img_spaceship);
-    game.enemy = createImage(img_enemy);
-    game.explosionEffect = createImage(img_explosion_effect);
+    cd_open();
+    cd_read_file("Explode1Right.VAG", &game.data[0]);   // Explosion sound
+    cd_read_file("ENEMY.TIM", &game.data[1]);           // Enemy sprite
+    cd_read_file("SHIP.TIM", &game.data[2]);            // Ship sprite
+    cd_read_file("EXPLOSION.TIM", &game.data[3]);       // Explosion sprite
+    cd_read_file("LASER-BOLTS.TIM", &game.data[4]);     // Laser sprite
+    cd_read_file("Hit_Hurt2Right.VAG", &game.data[5]);  // Hit sound
+    cd_read_file("Laser_002Right.VAG", &game.data[6]);  // Laser sound
+    cd_read_file("musicRight.VAG", &game.data[7]);      // Background music
+    cd_close();
 
-    game.explodeSound = loadAudio("assets/explode.wav");
-    game.hitHurtSound = loadAudio("assets/hit_hurt.wav");
-    game.laserSound = loadAudio("assets/laser.wav");
-    game.music = loadAudio("assets/music.wav");
+    audio_init();
+    sprite_create((u_char*)game.data[1], 32, 32, &game.enemy.sprite);
+    sprite_create((u_char*)game.data[2], 32, 32, &game.ship.sprite);
+    sprite_create((u_char*)game.data[3], 32, 32, &game.explosion.sprite);
+    sprite_create((u_char*)game.data[4], 16, 16, &game.laserBolt.sprite);
 
-    playAudio(game.music);
+    free3(game.data[0]);
+    free3(game.data[5]);
+    free3(game.data[6]);
 }
 
-void cleanupAssets() {
-    freeImage(game.spaceship);
-    freeImage(game.enemy);
-    freeImage(game.explosionEffect);
+void update() {
+    pad_update();
 
-    freeAudio(game.explodeSound);
-    freeAudio(game.hitHurtSound);
-    freeAudio(game.laserSound);
-    freeAudio(game.music);
+    // Ship movement
+    if (pad_check(Pad1Left) && game.ship.x > 0) {
+        game.ship.x -= 4;
+    }
+    if (pad_check(Pad1Right) && game.ship.x < SCREEN_WIDTH - 32) {
+        game.ship.x += 4;
+    }
+
+    // Fire bullet
+    if (pad_check(Pad1Cross)) {
+        fireBullet();
+    }
+
+    updateBullets();
+    updateEnemies();
+    checkCollisions();
 }
 
-void resetGame() {
-    game.x = SCREEN_WIDTH / 2;
-    game.y = SCREEN_HEIGHT - 50;
-    game.lives = 3;
-    game.score = 0;
-    game.gameState = STATE_MENU;
-}
+void draw() {
+    // Draw the player's ship
+    draw_sprite_at(game.ship.sprite, game.ship.x, game.ship.y);
 
-void drawMenu() {
-    clearDisplay();
-    setBackgroundColor(createColor(0, 0, 0)); // Dark background
-    drawText("Space Shooter", SCREEN_WIDTH / 2 - 50, SCREEN_HEIGHT / 2 - 20, createColor(255, 255, 255));
-    drawText("Press Start to Play", SCREEN_WIDTH / 2 - 70, SCREEN_HEIGHT / 2 + 10, createColor(255, 255, 255));
-    display();
-}
+    // Draw bullets
+    for (int i = 0; i < MAX_BULLETS; i++) {
+        if (game.bullets[i].active) {
+            draw_sprite_at(game.laserBolt.sprite, game.bullets[i].x, game.bullets[i].y);
+        }
+    }
 
-void handleInput() {
-    padUpdate();
-    if (padCheck(Pad1Start)) {
-        if (game.gameState == STATE_MENU) {
-            game.gameState = STATE_PLAY;
-        } else if (game.gameState == STATE_GAME_OVER) {
-            resetGame();
-            game.gameState = STATE_MENU;
+    // Draw enemies
+    for (int i = 0; i < MAX_ENEMIES; i++) {
+        if (game.enemies[i].active) {
+            draw_sprite_at(game.enemy.sprite, game.enemies[i].x, game.enemies[i].y);
         }
     }
 }
 
-void initializeBullets() {
-    for (int i = 0; i < MAX_BULLETS; i++) {
-        game.bullets[i].active = false;
-    }
-    game.bulletIndex = 0;
-}
-
-void initializeEnemies() {
-    for (int i = 0; i < MAX_ENEMIES; i++) {
-        game.enemies[i].active = false;
-    }
-    game.enemyIndex = 0;
-}
-
-void initializeExplosions() {
-    for (int i = 0; i < MAX_EXPLOSIONS; i++) {
-        game.explosions[i].active = false;
-        game.explosions[i].frame = 0;
-    }
-    game.explosionIndex = 0;
-}
-
 void fireBullet() {
-    if (!game.bullets[game.bulletIndex].active) {
-        game.bullets[game.bulletIndex].x = game.x;
-        game.bullets[game.bulletIndex].y = game.y;
-        game.bullets[game.bulletIndex].active = true;
-        playAudio(game.laserSound);
-        game.bulletIndex = (game.bulletIndex + 1) % MAX_BULLETS;
+    Bullet* bullet = &game.bullets[game.bulletIndex];
+    if (!bullet->active) {
+        bullet->x = game.ship.x + 16; // Center of the ship
+        bullet->y = game.ship.y;
+        bullet->active = true;
+
+        // Play laser sound
+        audio_transfer_vag_to_spu((u_char*)game.data[6], SECTOR * 21, SPU_02CH);
+        audio_play(SPU_02CH);
     }
+    game.bulletIndex = (game.bulletIndex + 1) % MAX_BULLETS;
 }
 
 void updateBullets() {
     for (int i = 0; i < MAX_BULLETS; i++) {
         if (game.bullets[i].active) {
-            game.bullets[i].y -= BULLET_SPEED;
+            game.bullets[i].y -= 5;
             if (game.bullets[i].y < 0) {
                 game.bullets[i].active = false;
             }
@@ -221,7 +183,7 @@ void updateBullets() {
 void updateEnemies() {
     for (int i = 0; i < MAX_ENEMIES; i++) {
         if (game.enemies[i].active) {
-            game.enemies[i].y += ENEMY_SPEED;
+            game.enemies[i].y += 2;
             if (game.enemies[i].y > SCREEN_HEIGHT) {
                 game.enemies[i].active = false;
             }
@@ -229,77 +191,26 @@ void updateEnemies() {
     }
 }
 
-void updateExplosions() {
-    for (int i = 0; i < MAX_EXPLOSIONS; i++) {
-        if (game.explosions[i].active) {
-            game.explosions[i].frame++;
-            if (game.explosions[i].frame > EXPLOSION_FRAME_COUNT) {
-                game.explosions[i].active = false;
-            }
-        }
-    }
-}
-
-void triggerExplosion(int x, int y) {
-    Explosion* exp = &game.explosions[game.explosionIndex];
-    exp->x = x;
-    exp->y = y;
-    exp->active = true;
-    exp->frame = 0;
-    playAudio(game.explodeSound);
-    game.explosionIndex = (game.explosionIndex + 1) % MAX_EXPLOSIONS;
-}
-
-void checkBulletCollisions() {
+void checkCollisions() {
     for (int i = 0; i < MAX_BULLETS; i++) {
         if (game.bullets[i].active) {
             for (int j = 0; j < MAX_ENEMIES; j++) {
-                if (game.enemies[j].active && checkCollision(game.enemies[j].x, game.enemies[j].y, ENEMY_WIDTH, ENEMY_HEIGHT, game.bullets[i])) {
+                if (game.enemies[j].active &&
+                    checkCollision(game.bullets[i].x, game.bullets[i].y, 16, 16,
+                                   game.enemies[j].x, game.enemies[j].y, 32, 32)) {
                     game.bullets[i].active = false;
                     game.enemies[j].active = false;
                     game.score += 10;
-                    triggerExplosion(game.enemies[j].x, game.enemies[j].y);
-                    break;
+
+                    // Play hit sound
+                    audio_transfer_vag_to_spu((u_char*)game.data[5], SECTOR * 21, SPU_03CH);
+                    audio_play(SPU_03CH);
                 }
             }
         }
     }
 }
 
-void drawGame() {
-    clearDisplay();
-    setBackgroundColor(createColor(0, 0, 0)); // Dark background
-    drawImage(game.spaceship, game.x, game.y);
-
-    for (int i = 0; i < MAX_BULLETS; i++) {
-        if (game.bullets[i].active) {
-            drawRectangle(game.bullets[i].x, game.bullets[i].y, 5, 10, createColor(255, 255, 0)); // Bullet
-        }
-    }
-
-    for (int i = 0; i < MAX_ENEMIES; i++) {
-        if (game.enemies[i].active) {
-            drawImage(game.enemy, game.enemies[i].x, game.enemies[i].y);
-        }
-    }
-
-    drawExplosions();
-
-    // Display score and lives
-    char buffer[50];
-    sprintf(buffer, "Score: %d", game.score);
-    drawText(buffer, 10, 10, createColor(255, 255, 255));
-
-    sprintf(buffer, "Lives: %d", game.lives);
-    drawText(buffer, 10, 30, createColor(255, 255, 255));
-
-    display();
-}
-
-void gameOver() {
-    clearDisplay();
-    setBackgroundColor(createColor(0, 0, 0)); // Dark background
-    drawText("Game Over", SCREEN_WIDTH / 2 - 50, SCREEN_HEIGHT / 2 - 20, createColor(255, 0, 0));
-    drawText("Press Start to Retry", SCREEN_WIDTH / 2 - 70, SCREEN_HEIGHT / 2 + 10, createColor(255, 255, 255));
-    display();
+bool checkCollision(int x1, int y1, int w1, int h1, int x2, int y2, int w2, int h2) {
+    return (x1 < x2 + w2 && x1 + w1 > x2 && y1 < y2 + h2 && y1 + h1 > y2);
 }
